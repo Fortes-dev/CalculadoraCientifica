94127a5390851331192afbd8016a0a3706c8dc57
Fortes <medina.fortes.carlos@gmail.com>
2021-02-01 20:52:47 +0100

    Version 4

--- main.java
+++ main.java
@@ -2,8 +2,16 @@
 public class main {
 
 	public static void main(String[] args) {
-
+		
 
 	}
-
+	
+	
+	
+	public static int operador (int resultado) {
+		Calculadora calculadora = new Calculadora(resultado);
+		
+		
+		return resultado;
+	}
 }
