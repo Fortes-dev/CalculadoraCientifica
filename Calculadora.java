c72033de84bb4cfa09646ecc47fa6e5439e0268a
Zenys <zeltax@hotmail.es>
2021-02-02 20:14:11 +0100

    version final

--- Calculadora.java
+++ Calculadora.java
@@ -3,57 +3,62 @@ public class Calculadora {
 	private double op;
 	private double num1;
 	private double num2;
-	
-	
-	
-	public Calculadora(double i) {//constructor de la clase calculadora
+
+	public Calculadora(double i) {// constructor de la clase calculadora
 		this.op = op;
 		this.num1 = num1;
 		this.num2 = num2;
-		
+
 	}
-	
+
 	public double getOp() {
 		return op;
 	}
-	
+
 	public double getNum1() {
 		return num1;
 	}
-	
+
 	public double getNum2() {
 		return num2;
 	}
-	
+
 	public void setOp(double op) {
 		this.op = op;
 	}
-	
+
 	public void setNum1(double num1) {
 		this.num1 = num1;
 	}
-	
+
 	public void setNum2(double num2) {
 		this.num2 = num2;
 	}
-	
-	public double operacion (String operacion) {//Funcion para operar.
-		
-		switch(operacion) {
-		
-		case "+": op = num1+num2;
-		case "-": op = num1-num2;
-		case "*": op = num1*num2;
-		case "/": op = num1/num2;
-		case "sqrt": op = Math.sqrt(op);
-		case "sin": op = Math.sin(op);
-		case "cos": op = Math.cos(op);
-		case "inv": op = 1/num1;
-		
+
+	public double operacion(String operacion) { // Funcion para operar.
+
+		switch (operacion) {
+
+		case "+":
+			op = num1 + num2;
+		case "-":
+			op = num1 - num2;
+		case "*":
+			op = num1 * num2;
+		case "/":
+			op = num1 / num2;
+		case "sqrt":
+			op = Math.sqrt(op);
+		case "sin":
+			op = Math.sin(op);
+		case "cos":
+			op = Math.cos(op);
+		case "inv":
+			op = 1 / num1;
+
 		}
-		
+
 		return op;
 	}
-	
-	
+
 }
--- main.java
+++ main.java
@@ -1,17 +1,48 @@
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
-		
-
+		Calculadora calculadora = new Calculadora(0);
+		Scanner sc = new Scanner(System.in);
+		informacion();
+		double num1 = sc.nextDouble();
+		String[] a = "10 + 10".split(" ");
+		if(a.length == 3) {
+			Double.parseDouble(s)
+		}
+		else throw IllegalArgumentException();
 	}
-	
-	
-	
-	public static int operador (int resultado) {
-		Calculadora calculadora = new Calculadora(resultado);
-		
-		
-		return resultado;
+
+	public static void informacion() {
+		System.out.println("Para realizar los calculos matemáticos utilice:");
+		System.out.println("+ para sumar");
+		System.out.println("- para resta");
+		System.out.println("* para multiplicar");
+		System.out.println("/ para dividir");
+		System.out.println("sqrt para la raiz cuadrada");
+		System.out.println("cos para el coseno");
+		System.out.println("sin para el seno");
+		System.out.println("inv para el inverso");
+	}
+
+	public static void detectarNumero(String cadena) {
+		String numero = "";
+		char operador = '';
+		String num2;
+		for (int i = 0; i < cadena.length(); i++) {
+			if (cadena.charAt(i) >= '0' && cadena.charAt(i) <= 9) {
+				while (i < cadena.length() && cadena.charAt(i) >= '0' && cadena.charAt(i) <= '9') {
+					numero = numero + cadena.charAt(i);
+					i++;
+				}
+			}
+			
+			if (cadena.charAt(i) == '+' || cadena.charAt(i) == '-' || cadena.charAt(i) == '*' || cadena.charAt(i) == '/' ||) {
+				operador = cadena.charAt(i);
+				num2 = cadena.substring(i+1);
+				break;
+			}
+		}
 	}
 }
